d1fc78ed3ea2aa676e95c5dc97ab5f77f2507046
dancastillo <dancastillo99@gmail.com>
2017-11-06 19:09:25 -0500

    app.js: tuned route /api/users

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use(bodyParser.urlencoded({'extended': true}));
 app.use(bodyParser.json());
 
 
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const routes = require("./api/routes/routes");
 
-app.use('/', routes);
+app.use('/api', routes);
 
 const server = http.createServer(app);
 
