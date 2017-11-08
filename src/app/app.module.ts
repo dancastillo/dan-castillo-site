2ca94e4b73c08231a0a2b1c7ab6f370b7b6e791e
dancastillo <dancastillo99@gmail.com>
2017-11-08 10:12:32 -0500

    app.module.ts: added SkillsServe to provides and SkillCardComponent to declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 
 import { HttpModule } from '@angular/http';
 import { DataService } from './services/data.service';
+import { SkillsService } from './services/skills.service';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EmploymentComponent } from './employment/employment.component';
 import { SkillsComponent } from './skills/skills.component';
 import { EducationComponent } from './education/education.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
+import { SkillCardComponent } from './skill-card/skill-card.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +22,17 @@ import { ContactComponent } from './contact/contact.component';
     SkillsComponent,
     EducationComponent,
     PortfolioComponent,
-    ContactComponent
+    ContactComponent,
+    SkillCardComponent
   ],
   imports: [
     BrowserModule,
     HttpModule
   ],
-  providers: [ DataService ],
+  providers: [ 
+    DataService,
+    SkillsService
+   ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
