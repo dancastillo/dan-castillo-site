5871d458904413270f1583872334f93bd7932dcb
dancastillo <dancastillo99@gmail.com>
2017-11-15 13:12:35 -0500

    routes.js: added route for getting school information

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,8 +5,12 @@ const Users = require('./../controllers/userController.js');
 
 const Skills = require('./../controllers/skillController.js');
 
+const Schools = require('./../controllers/schoolController.js');
+
 routes.get('/users', Users.get_all_users);
 
 routes.get('/skills', Skills.get_all_skills);
+
+routes.get('/schools', Schools.get_all_skills);
 
 module.exports = routes;
