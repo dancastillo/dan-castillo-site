193900db299f34834c9ec8d991dee91382111302
dancastillo <dancastillo99@gmail.com>
2017-11-14 22:54:48 -0500

    routes.js: created a route for Skills

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,6 +3,10 @@ const routes = express();
 
 const Users = require('./../controllers/userController.js');
 
+const Skills = require('./../controllers/skillController.js');
+
 routes.get('/users', Users.get_all_users);
+
+routes.get('/skills', Skills.get_all_skills);
 
 module.exports = routes;
