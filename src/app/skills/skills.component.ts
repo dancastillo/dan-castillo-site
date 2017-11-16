35152b53b9db276ea6388aa45347d74e28fff393
dancastillo <dancastillo99@gmail.com>
2017-11-16 15:42:26 -0500

    skills.component.ts: tuned import of skillsservice

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SkillsService} from './../services/skills.service';
+import { SkillsService} from './../services/skills/skills.service';
 
 @Component({
   selector: 'app-skills',
