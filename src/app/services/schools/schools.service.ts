7d27a2c90d8086326d99c4e9d2ecd017d53510d5
dancastillo <dancastillo99@gmail.com>
2017-11-16 13:05:22 -0500

    schools.service: added service to get data from http

---
+++ src/app/services/schools/schools.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class SchoolsService {
+
+  constructor(private _http: Http) { }
+
+  getSchools() {
+    return this._http.get('api/schools').map(res => res.json());
+  }
+
+}
