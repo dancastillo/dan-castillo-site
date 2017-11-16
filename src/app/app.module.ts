5cd320d90e2cf717ce0cb80bfa42b1986346abd8
dancastillo <dancastillo99@gmail.com>
2017-11-16 15:40:57 -0500

    app.module.ts: tuned imports skill-card new location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 
 import { HttpModule } from '@angular/http';
 import { DataService } from './services/data.service';
-import { SkillsService } from './services/skills.service';
+import { SkillsService } from './services/skills/skills.service';
 import { SchoolsService } from './services/schools/schools.service';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EmploymentComponent } from './employment/employment.component';
@@ -13,7 +13,7 @@ import { SkillsComponent } from './skills/skills.component';
 import { EducationComponent } from './education/education.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
-import { SkillCardComponent } from './skill-card/skill-card.component';
+import { SkillCardComponent } from './skills/skill-card/skill-card.component';
 import { JobsService } from './services/jobs/jobs.service';
 
 @NgModule({
