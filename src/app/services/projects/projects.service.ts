a1b5382160d808350fb55d195ec7f076af91c638
dancastillo <dancastillo99@gmail.com>
2017-11-22 10:15:40 -0500

    projects.service.ts: added http request to get project colleciton

--- src/app/services/projects/projects.service.ts
+++ src/app/services/projects/projects.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProjectsService {
 
-  constructor() { }
+  constructor(private _http: Http) { }
 
+  getProjects() {
+    return this._http.get('api/projects').map(res => res.json());
+  }
 }
