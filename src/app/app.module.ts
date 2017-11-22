fbff10e15ec32a5a9efd90e84225d78bd1c7e790
dancastillo <dancastillo99@gmail.com>
2017-11-22 10:19:09 -0500

    app.module.ts: tidy file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
+// imports
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+
+// services
 import { DataService } from './services/data.service';
 import { SkillsService } from './services/skills/skills.service';
 import { SchoolsService } from './services/schools/schools.service';
+import { JobsService } from './services/jobs/jobs.service';
+import { ProjectsService } from './services/projects/projects.service';
+
+// components
 import { NavbarComponent } from './navbar/navbar.component';
 import { SkillsComponent } from './skills/skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactComponent } from './contact/contact.component';
 import { SkillCardComponent } from './skills/skill-card/skill-card.component';
-import { JobsService } from './services/jobs/jobs.service';
+import { ContactComponent } from './contact/contact.component';
 import { SchoolComponent } from './school/school.component';
 import { JobComponent } from './job/job.component';
 import { ProjectComponent } from './project/project.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SkillsComponent,
-    PortfolioComponent,
     ContactComponent,
     SkillCardComponent,
     SchoolComponent,
@@ -37,7 +41,8 @@ import { ProjectComponent } from './project/project.component';
     DataService,
     SkillsService,
     SchoolsService,
-    JobsService
+    JobsService,
+    ProjectsService
    ],
   bootstrap: [ AppComponent ]
 })
