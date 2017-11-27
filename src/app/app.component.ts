08b6549a2c9d9305e38c7513dc752577da2498dc
dancastillo <dancastillo99@gmail.com>
2017-11-27 15:47:42 -0500

    app.component.ts: added locations for contact photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ import { DataService } from './services/data.service';
 export class AppComponent {
 
   users: Array<string>;
+  email: string = 'http://res.cloudinary.com/dancastillo/image/upload/v1511814580/email_ado8sw.png';
+  linkedin: string ='http://res.cloudinary.com/dancastillo/image/upload/v1511814584/linkedin_zmog3d.png';
+  github: string ='http://res.cloudinary.com/dancastillo/image/upload/v1511814583/github_ogfwrd.png';
 
   // Create an instance of the DataService through dependency injection
   constructor(private _dataService: DataService) {
