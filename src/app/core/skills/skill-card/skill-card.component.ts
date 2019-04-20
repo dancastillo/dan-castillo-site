b5761f20ae260c472f923c57d8f2449b0b0d03b8
dancastillo <dancastillo99@gmail.com>
2019-04-20 12:56:04 -0400

    install angular/material via npm install

---
+++ src/app/core/skills/skill-card/skill-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-card',
+  templateUrl: './skill-card.component.html',
+  styleUrls: ['./skill-card.component.scss']
+})
+export class SkillCardComponent implements OnInit {
+
+  // @Input('skill') skill: Array<string>;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
